e408ec9defa13199ebcb50b878ec4415ecd0ea7e
Connor McGarty <cmcgarty@uab.edu>
2020-02-13 19:31:12 -0600

    initial commit

---
+++ scan.js
@@ -0,0 +1,41 @@
+const _ = require('lodash')
+const BeaconScanner = require('node-beacon-scanner');
+const noble = require('@abandonware/noble');
+const http = require('http');
+const scanner = new BeaconScanner({'noble': noble});
+
+let advertisements = []; // beacons
+
+const scanInterval = 2; // seconds
+
+const collectBeacon = (ad) => {
+
+};
+
+// received an advertisement packet
+scanner.onadvertisement = ad => {
+	console.log(JSON.stringify(ad, null, ' ')); // log to console
+	advertisements.push(ad); // add to advertisements collection
+};
+
+let interval = setInterval(collectRSSIs, scanInterval * 1000);
+
+// called every interval:
+//		- separate out unique beacons
+//		- for each unique beacon, get all rssi's received
+//		- average rssi's for each beacon
+//		- empty advertisements[]
+const collectRSSIs = () => {
+	uniq_beacons = _.find(_.countBy(advertisements, (advertisements) => {return advertisements.iBeacon.UUID})); // should return list of UUIDs received
+	beacons = _.filter(advertisements, () => {
+	});
+};
+
+
+// start scanning for iBeacon advertisements
+scanner.startScan().then(() => {
+	console.log('Started scan');
+}).catch(error => {
+	console.log(error);
+});
+;
